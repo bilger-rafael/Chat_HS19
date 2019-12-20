6dd4a1a84590f45a28aec822d1d32e38ea78f6c7
Etter <dominic.etter@students.fhnw.ch>
2019-12-20 14:21:39 +0100

    RunLater BaktoLoginView

--- src/chat/NewUserClasses/NewUserController.java
+++ src/chat/NewUserClasses/NewUserController.java
@@ -17,31 +17,29 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.stage.WindowEvent;
 
-
 public class NewUserController extends Controller<NewUserModel, NewUserView> {
 	ServiceLocator serviceLocator;
 	private int positionInArray = 0;
 
+	public NewUserController(NewUserModel model, NewUserView view) {
+		super(model, view);
 
-    public NewUserController(NewUserModel model, NewUserView view) {
-        super(model, view);
-        
-        //Action für CreateUserButton
-        view.getCancelButton().setOnAction( e -> getBackLoginView());
-        
-        //Action für CreateUserButton
-        view.getOkButton().setOnAction( e -> createUser());
-        
-        // register ourselves to handle window-closing event
-        view.getStage().setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                Platform.exit();
-            }
-          
-        });
-        
-      // Validierung der Passwort-Eingabe
+		// Action für CreateUserButton
+		view.getCancelButton().setOnAction(e -> getBackLoginView());
+
+		// Action für CreateUserButton
+		view.getOkButton().setOnAction(e -> createUser());
+
+		// register ourselves to handle window-closing event
+		view.getStage().setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+				Platform.exit();
+			}
+
+		});
+
+		// Validierung der Passwort-Eingabe
 		view.getPwField().textProperty().addListener(new ChangeListener<String>() {
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
@@ -49,18 +47,18 @@ public class NewUserController extends Controller<NewUserModel, NewUserView> {
 
 				int i = view.getPwField().getText().length();
 
-				if( i < 3 ) {
+				if (i < 3) {
 					view.getPwField().getStyleClass().remove("pwField-green");
 					view.getPwField().getStyleClass().add("pwField-red");
-				}else {
+				} else {
 					view.getPwField().getStyleClass().remove("pwField-red");
 					view.getPwField().getStyleClass().add("pwField-green");
 					view.getOkButton().setDisable(false);
 				}
 			}
 		});
-		
-		//Validierung Namenseingabe
+
+		// Validierung Namenseingabe
 		view.getNameField().textProperty().addListener(new ChangeListener<String>() {
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
@@ -68,80 +66,80 @@ public class NewUserController extends Controller<NewUserModel, NewUserView> {
 
 				int i = view.getNameField().getText().length();
 
-				if( i < 3 ) {
+				if (i < 3) {
 					view.getNameField().getStyleClass().remove("nameField-green");
 					view.getNameField().getStyleClass().add("nameField-red");
-				}else {
+				} else {
 					view.getNameField().getStyleClass().remove("nameField-red");
 					view.getNameField().getStyleClass().add("nameField-green");
 					view.getOkButton().setDisable(false);
 				}
 			}
 		});
-		
-		//Schliesst Sitzung falls das Fenster geschlossen wird
-        view.getStage().setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                view.stop();
-                Platform.exit();
-            }
-        });
-        
-        serviceLocator = ServiceLocator.getServiceLocator();        
-        serviceLocator.getLogger().info("Application controller initialized");
-    }
-    
-    //Leitet zur LoginView nach Abbruch
-    private void getBackLoginView() {
-    	//Logik für zurück auf LoginView
-    	this.view.stop();
-    	JavaFX_App_Template.getMainProgram().getLoginView().start();
-		
-    }
-    //Erstellt User beim Server und leitet zur LoginView
-    private void createUser() {
-    	String username = view.getNameField().getText();
-    	String password = view.getPwField().getText();
-    	
-    	CreateLogin createLogin = new CreateLogin(username, password);
-    	
-    	
-    	this.positionInArray = Client.getClient().addMsgListener((Message msg) -> {
-    		if(msg instanceof Result) {
-				Result r = (Result)msg;
-				if( r.getType() == ResultType.Simple ) {
-					if(r.getBoolean()) {
+
+		// Schliesst Sitzung falls das Fenster geschlossen wird
+		view.getStage().setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+				view.stop();
+				Platform.exit();
+			}
+		});
+
+		serviceLocator = ServiceLocator.getServiceLocator();
+		serviceLocator.getLogger().info("Application controller initialized");
+	}
+
+	// Leitet zur LoginView nach Abbruch
+	private void getBackLoginView() {
+		// Logik für zurück auf LoginView
+		this.view.stop();
+		JavaFX_App_Template.getMainProgram().getLoginView().start();
+
+	}
+
+	// Erstellt User beim Server und leitet zur LoginView
+	private void createUser() {
+		String username = view.getNameField().getText();
+		String password = view.getPwField().getText();
+
+		CreateLogin createLogin = new CreateLogin(username, password);
+
+		this.positionInArray = Client.getClient().addMsgListener((Message msg) -> {
+			if (msg instanceof Result) {
+				Result r = (Result) msg;
+				if (r.getType() == ResultType.Simple) {
+					if (r.getBoolean()) {
 						serviceLocator.getLogger().info("erstellt");
-						backLoginViewAfterLogin();
-				
-					}else {
+						Platform.runLater(() -> {
+							backLoginViewAfterLogin();
+						});
+
+					} else {
 						JavaFX_App_Template.getMainProgram().getNewUserView().setErrorLabel();
 						serviceLocator.getLogger().info("User gibt es schon");
 					}
 					Client.getClient().removeMsgListener(Client.getClient().getMessageListener(positionInArray));
 				}
 			}
-    	});
-    	
-    	Client.getClient().send(createLogin);
-    	
-    }
-    
-    //Leitet zur Loginview nach dem Login
-    private void backLoginViewAfterLogin() {
-    	
-    	this.view.stop();
-    	setConnectedInLoginView();
-    	JavaFX_App_Template.getMainProgram().getLoginView().start();
-    	
-    	
-    }
-    
-    //Setzt den Text Connected auf der Login View
-    private void setConnectedInLoginView() {
-    	JavaFX_App_Template.getMainProgram().getLoginView().setConnectedLabel();
-    }
-    
+		});
+
+		Client.getClient().send(createLogin);
+
+	}
+
+	// Leitet zur Loginview nach dem Login
+	private void backLoginViewAfterLogin() {
+
+		this.view.stop();
+		setConnectedInLoginView();
+		JavaFX_App_Template.getMainProgram().getLoginView().start();
+
+	}
+
+	// Setzt den Text Connected auf der Login View
+	private void setConnectedInLoginView() {
+		JavaFX_App_Template.getMainProgram().getLoginView().setConnectedLabel();
+	}
 
 }
